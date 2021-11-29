44daa2c0726840b15cb1d4abd57b59fdc33ebaea
Ousseynou <osakho62@gmail.com>
2021-11-29 11:02:21 +0100

    change file

--- file.c
+++ file.c
@@ -8,12 +8,34 @@ typedef struct maillon
 }cellule;
 
 
-/*typedef struct file{
-	cellule * 
-}file ;*/
 
 void initialiser (cellule * file){
 	file -> tete = NULL ;
 }
 
-int top_file ()
+int top_file (cellule * file, int  * top){
+	if (estVide (file) == 1){
+		printf("Pile vide \n");
+		return 0;
+	}
+	else{
+		*top = file -> top ;
+		return 1 ;
+	}
+}
+
+int enfiler (cellule * file, int * donnee){
+	if(estPleine (file) == 1){
+		printf("Pile pleine : Error \n") ;
+		return 0 ;
+	}
+	else{
+		//*donnee = p -> 
+	}
+}
+
+int defiler (cellule* file, int* donnee){
+	return 0;
+}
+int estPleine (cellule* file){}
+int estVide  (cellule * file){}
