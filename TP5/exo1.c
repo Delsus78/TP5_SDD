48e56bf9d4ca1773eabb50a09fc82cd99aee73c1
Delsus78 <delsustheugm@gmail.com>
2021-11-30 12:52:22 +0100

    create folder

---
+++ TP5/exo1.c
@@ -0,0 +1,20 @@
+#include "file.c"
+
+tri(Patient) {
+
+}
+
+int main(int argc, char const *argv[])
+{
+    // init Patient
+    Patient p1 = malloc(sizeof(Patient));
+    Patient p2 = malloc(sizeof(Patient));
+    Patient p3 = malloc(sizeof(Patient));
+    Patient p4 = malloc(sizeof(Patient));
+    Patient p5 = malloc(sizeof(Patient));
+    
+    // init valeurs temps
+
+
+    return 0;
+}
