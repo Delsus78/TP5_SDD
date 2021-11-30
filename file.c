344e1b508a7a75479e066f1f51959f1677fca6a6
Ousseynou <osakho62@gmail.com>
2021-11-30 12:50:51 +0100

    changes

--- file.c
+++ file.c
@@ -1,30 +1,36 @@
 #include<stdio.h>
 
+typedef struct client{
+	char nom [10] ;
+	int t_arrivee ;
+	int t_traitement ;
+}client ;
+
+
 typedef struct maillon
 {
 	struct maillon* tete ;
 	struct maillon* queue ;
-	float top ;
+	client top ;
 }cellule;
-
 
 
 void initialiser (cellule * file){
 	file -> tete = NULL ;
 }
 
-int top_file (cellule * file, int  * top){
+int top_file (cellule * file, client  * next){
 	if (estVide (file) == 1){
 		printf("Pile vide \n");
 		return 0;
 	}
 	else{
-		*top = file -> top ;
+		*next = file -> top ;
 		return 1 ;
 	}
 }
 
-int enfiler (cellule * file, int * donnee){
+int enfiler (cellule * file, client * new){
 	if(estPleine (file) == 1){
 		printf("Pile pleine : Error \n") ;
 		return 0 ;
@@ -34,8 +40,9 @@ int enfiler (cellule * file, int * donnee){
 	}
 }
 
-int defiler (cellule* file, int* donnee){
-	return 0;
+int defiler (cellule* file, client* donnee){
+
+		return 0;
 }
 int estPleine (cellule* file){}
 int estVide  (cellule * file){}
